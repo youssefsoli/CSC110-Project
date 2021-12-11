7528237f8826c4266888531b7c137abc5f1ec826
youssefsoli <youssefdevelops@gmail.com>
2021-12-11 15:57:26 -0500

    Updated plotting

--- plot.py
+++ plot.py
@@ -1,4 +1,14 @@
 """
+Plotting
 """
 
+import plotly.express as px
+import pandas as pd
+from parse import load_data
 
+
+if __name__ == '__main__':
+    housing_data = load_data('House_Price_Index.csv')
+    df = pd.DataFrame(housing_data['c11'])
+    fig = px.line(df, x="transaction_date", y="index", title="Unsorted Input")
+    fig.show()
