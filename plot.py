860b902bcb68787eaaf417390b5276ad3f3eff42
MinGi-K <64427415+MinGi-K@users.noreply.github.com>
2021-12-11 20:00:24 -0500

    Implemented manual regression

--- plot.py
+++ plot.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     df = pd.DataFrame(housing_data['bc_victoria'])
     df['transaction_date'] = df['transaction_date'].apply(regression.calculate_days)
 
-    # create fig2
+    # create line_regression_dataframe
     x_values = range(0, 11386)
     slope = regression.calculate_regression_slope(housing_data['bc_victoria'])
     intercept = regression.calculate_regression_intercept(housing_data['bc_victoria'], slope)
@@ -22,9 +22,8 @@ if __name__ == '__main__':
     df2 = pd.DataFrame()
     df2['x'] = x_values
     df2['y'] = y_values
-    # fig2 = px.line(df2, x='x', y='y', title='test')
 
-    # help me
+    # plot both figs on same axis
     fig = px.line(df, x="transaction_date", y="index", title="Unsorted Input")
     fig.add_scatter(x=df2['x'], y=df2['y'])
     fig.show()
