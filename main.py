20793ab649829f5809e32b6bc17dda35e47f3c93
MinGi-K <64427415+MinGi-K@users.noreply.github.com>
2021-12-11 21:35:32 -0500

    changed regression functions to accept df parameters.

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 """
 MAIN
 """
+from housing_entry import IndexData
+import regression
+import parse
+import train_test_data
+import regression
+
+# load data
+data = parse.load_data('House_Price_Index.csv')
+
+# get test_and_training_data
+tt_data = train_test_data.get_train_test_data(data)
+
+# get linear regression line from train data:
+regression_dict = {}
+
+for location in tt_data:
+    regression_dict[location] = regression.least_squares_regression(tt_data[location][0])
