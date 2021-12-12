aafae86cbb793cb159259107647e1f07b6e5bf5a
youssefsoli <youssefdevelops@gmail.com>
2021-12-11 21:31:33 -0500

    Added copyright information

--- parse.py
+++ parse.py
@@ -1,5 +1,15 @@
-"""
-This file is used to parse the house price index data
+"""This file is used to parse the house price index data
+
+Copyright and Usage Information
+===============================
+
+This file is provided solely for marking purposes for the CSC110 instruction
+and marking team at the University of Toronto St. George campus. All forms of
+distribution of this code, whether as given or with any changes, are
+expressly prohibited. For more information on copyright for this CSC110
+project, please consult one of the team members.
+
+This file is Copyright (c) 2021 Aidan Li, Youssef Soliman, Min Gi Kwon, and Tej Jaspal Capildeo.
 """
 
 import csv
