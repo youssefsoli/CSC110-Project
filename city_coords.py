7f7b5767802163e5ef5ac54d0e7bf1cd5ec3081b
youssefsoli <youssefdevelops@gmail.com>
2021-12-11 20:15:17 -0500

    Added constant dictionary for city coordinates

---
+++ city_coords.py
@@ -0,0 +1,18 @@
+"""
+Holds city cooridnates
+Sourced from: https://www.latlong.net/
+"""
+
+COORDS = {
+    'bc_victoria': (48.407326, -123.329773),
+    'bc_vancouver': (49.246292, -123.116226),
+    'ab_calgary': (51.049999, -114.066666),
+    'ab_edmonton': (53.631611, -113.323975),
+    'mb_winnipeg': (49.895077, -97.138451),
+    'on_hamilton': (43.255203, -79.843826),
+    'on_toronto': (43.683334, -79.766670),
+    'on_ottawa': (45.424721, -75.695000),
+    'qc_montreal': (45.508888, -73.561668),
+    'qc_quebec_city': (46.829853, -71.254028),
+    'ns_halifax': (44.651070, -63.582687)
+}
