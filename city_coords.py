74b8a2db0f2f481084a99cfca9073efb4bceccbb
youssefsoli <youssefdevelops@gmail.com>
2021-12-14 07:33:00 -0500

    Added python_ta

--- city_coords.py
+++ city_coords.py
@@ -12,6 +12,7 @@ project, please consult one of the team members.
 
 This file is Copyright (c) 2021 Aidan Li, Youssef Soliman, Min Gi Kwon, and Tej Jaspal Capildeo.
 """
+import python_ta
 
 COORDS = {
     'bc_victoria': (48.407326, -123.329773),
@@ -26,3 +27,11 @@ COORDS = {
     'qc_quebec_city': (46.829853, -71.254028),
     'ns_halifax': (44.651070, -63.582687)
 }
+
+if __name__ == '__main__':
+    python_ta.check_all(config={
+        'extra-imports': [],  # the names (strs) of imported modules
+        'allowed-io': [],  # the names (strs) of functions that call print/open/input
+        'max-line-length': 100,
+        'disable': ['R1705', 'C0200']
+    })
